8b1134c8848107a7ae75c1b567453fcd48ef4020
MiguelAZM <miguelangelzm.006@gamil>
2024-03-08 20:54:30 -0500

    feat: :white_check_mark: pueba de archivos

--- class/cuenta.php
+++ class/cuenta.php
@@ -1,7 +1,17 @@
 <?php
 class Cuenta
 {
-    $nombre, 
+    private $nombre;
+    private $nrocuenta; private 
+    $tipointeres; 
+    private $saldo;
+    public function__contruct ($nombre, $nrocuenta,$saldo)
+    {
+        $this-> nombre = $nombre;
+        $this->cuenta= $nrocuenta;
+        $this -> tipointeres= $tipointeres;
+        $this-> saldo =$saldo;
+    }
 }
 
 
