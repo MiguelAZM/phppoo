fccbc4feb9d40e9baef8df716cfa01b0db681660
MiguelAZM <miguelangelzm.006@gamil>
2024-03-13 19:42:53 -0500

    actualizacion

--- class/cuenta.php
+++ class/cuenta.php
@@ -17,7 +17,53 @@ class Cuenta
     public function setNombre ($nombre){
         $this->nombre=$nombre;
     }
+
+
+    public function setNrcuenta ($nrocuenta){
+        $this->nrocuenta=$nrocuenta;
+    }
+
+    public function setTipointeres ($tipointeres){
+        $this->tipointeres=$tipointeres;
+    }
+
+    public function setSaldo ($saldo){
+        $this->saldo=$saldo;
+    }
+    public function getNombre(){
+        return $this->nombre;
+    }
+
+    public function getNrcuenta(){
+        return $this->nrocuenta;
+
+    }
+    public function getTipointeres(){
+        return $this->tipointeres;
+    }
+
+    public function getSaldo(){
+        return $this->saldo;
+    } 
+
+    public function descipcion(){
+        echo"titular de la cuenta:". $this->nombre."<br>";
+        echo"numero de cuenta:". $this->nrocuenta."<br>";
+        echo"tipo de interes:". $this->tipointeres."<br>";
+        echo"Saldo es :". $this->saldo."<br>";
+    }
+
+    public function ingreso($monto){
+        $ingresocorrecto=0;
+        if ($monto <=0){
+            $ingresocorrecto= false;
+        }else{
+            $this->saldo+=$monto;
+        }
+
+    }
 }
+
 
 
 ?>
